2c82b954b7a8db4701bfedd1d18faaf63f21b9c3
jrvieirapr <juliano.vieira@docente.pr.senac.br>
2023-07-13 15:13:19 -0300

    Ajuste Link

--- resources/views/tipos_bichos/index.blade.php
+++ resources/views/tipos_bichos/index.blade.php
@@ -1,3 +1,28 @@
 @extends('index')
 @section('content')
+<div>
+    <h1>Tipos Bichos</h1>
+    <a class="btn btn-primary" href="{{ route('tiposbichos.create') }} " role="button">Novo</a>
+    <div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Descrição</th>
+                    <th scope="col" colspan="2">Opções</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($tipos_bichos as $t)
+                <tr>
+                    <th scope="row">{{ $t->id}}</th>
+                    <td>{{$t->descricao}}</td>
+                    <td>Editar</td>
+                    <td>Remover</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('home');
 
 Route::resource('tiposbichos',TipoBichoController::class);
 Route::resource('bichos',BichoController::class);
