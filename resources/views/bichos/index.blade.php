d44a3849f27a38f53841419c615c7d01974df540
jrvieirapr <juliano.vieira@docente.pr.senac.br>
2023-07-19 14:50:07 -0300

    Index Bichos

---
+++ resources/views/bichos/index.blade.php
@@ -0,0 +1,44 @@
+@extends('index')
+@section('content')
+<div>
+    <h1>Bichos</h1>
+    <a class="btn btn-primary" href="{{ route('bichos.create') }}" role="button">Novo</a>
+    <div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Idade</th>
+                    <th scope="col">Cor</th>
+                    <th scope="col">Tipo</th>
+                    <th scope="col" colspan="2">Opções</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($bichos as $b)
+                <tr>
+                    <td scope="row">{{ $b->id}}</td>
+                    <td>{{$b->nome}}</td>
+                    <td>{{$b->idade}}</td>
+                    <td>{{$b->cor}}</td>
+                    <td>Tipo Bicho</td>
+                    <td>
+                        <div class="btn-group">
+                            <a class="btn btn-warning me-md-2 btn-formulario" 
+                            href="{{ route('bichos.edit', ['tiposbicho' => $b]) }}" role="button">Editar</a>
+                            
+                            <form action="{{ route('bichos.destroy', ['tiposbicho' => $b]) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-danger me-md-2 btn-formulario" type="submit">Remover</button>
+                            </form>
+                        </div>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+@endsection
